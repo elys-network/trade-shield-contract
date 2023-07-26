951143107a42ac4ff1c4e22d97db235bb74b2d6d
politeWall <138504353+politeWall@users.noreply.github.com>
2023-07-26 16:19:44 +0200

    [+] create order test [+] cancel order refund test

--- src/tests/cancel_order.rs
+++ src/tests/cancel_order.rs
@@ -2,10 +2,19 @@ use crate::{
     contract::*,
     error::ContractError,
     msg::{ExecuteMsg, InstantiateMsg},
-    state::Order,
+    state::{Order, OrderType},
 };
-use cosmwasm_std::Addr;
+use cosmwasm_std::{coins, Addr, Event};
 use cw_multi_test::{App, ContractWrapper, Executor};
+
+fn get_attribute_value(events: &Vec<Event>, key: &str) -> Option<String> {
+    for event in events {
+        if let Some(attr) = event.attributes.iter().find(|attr| attr.key == key) {
+            return Some(attr.value.clone());
+        }
+    }
+    None
+}
 
 #[test]
 fn not_found() {
@@ -82,5 +91,69 @@ fn unauthorized() {
             sender: Addr::unchecked("not_user")
         },
         err.downcast().unwrap()
+    );
+}
+
+#[test]
+fn cancel_order() {
+    let mut app = App::new(|router, _, storage| {
+        router
+            .bank
+            .init_balance(storage, &Addr::unchecked("user"), coins(150, "eth"))
+            .unwrap()
+    });
+
+    let code = ContractWrapper::new(execute, instantiate, query);
+    let code_id = app.store_code(Box::new(code));
+
+    let addr = app
+        .instantiate_contract(
+            code_id,
+            Addr::unchecked("owner"),
+            &InstantiateMsg { orders: vec![] },
+            &[],
+            "Contract",
+            None,
+        )
+        .unwrap();
+
+    let resp = app
+        .execute_contract(
+            Addr::unchecked("user"),
+            addr.clone(),
+            &ExecuteMsg::CreateOrder {
+                order_type: OrderType::TakeProfit,
+                stop_price: 255,
+                selling_denom: "btc".to_owned(),
+            },
+            &coins(45, "eth"),
+        )
+        .unwrap();
+    let id = get_attribute_value(&resp.events, "order_id").unwrap();
+
+    app.execute_contract(
+        Addr::unchecked("user"),
+        addr.clone(),
+        &ExecuteMsg::CancelOrder { order_id: id },
+        &[],
+    )
+    .unwrap();
+
+    assert_eq!(
+        app.wrap()
+            .query_balance("user", "eth")
+            .unwrap()
+            .amount
+            .u128(),
+        150
+    );
+
+    assert_eq!(
+        app.wrap()
+            .query_balance(&addr, "eth")
+            .unwrap()
+            .amount
+            .u128(),
+        0
     );
 }
--- src/tests/create_order.rs
+++ src/tests/create_order.rs
@@ -4,17 +4,14 @@ use crate::{
     msg::{ExecuteMsg, InstantiateMsg},
     state::OrderType,
 };
-use cosmwasm_std::Addr;
+use cosmwasm_std::{coins, Addr, Event};
 use cw_multi_test::{App, ContractWrapper, Executor};
 
-
-#[test] 
+#[test]
 fn coin_number() {
     let mut app = App::default();
 
-    let instantiate_msg = InstantiateMsg {
-        orders: vec![],
-    };
+    let instantiate_msg = InstantiateMsg { orders: vec![] };
     let code = ContractWrapper::new(execute, instantiate, query);
     let code_id = app.store_code(Box::new(code));
     let addr = app
@@ -28,13 +25,76 @@ fn coin_number() {
         )
         .unwrap();
     let err = app
-    .execute_contract(
-        Addr::unchecked("user"),
-        addr,
-        &ExecuteMsg::CreateOrder { order_type: OrderType::StopLoss, stop_price: 100 , selling_denom: "eth".to_owned() },
-        &[],
-    )
-    .unwrap_err();
+        .execute_contract(
+            Addr::unchecked("user"),
+            addr,
+            &ExecuteMsg::CreateOrder {
+                order_type: OrderType::StopLoss,
+                stop_price: 100,
+                selling_denom: "eth".to_owned(),
+            },
+            &[],
+        )
+        .unwrap_err();
 
     assert_eq!(ContractError::CoinNumber, err.downcast().unwrap());
+}
+
+#[test]
+fn create_order() {
+    let mut app = App::new(|router, _, storage| {
+        router
+            .bank
+            .init_balance(storage, &Addr::unchecked("user"), coins(150, "eth"))
+            .unwrap()
+    });
+
+    let code = ContractWrapper::new(execute, instantiate, query);
+    let code_id = app.store_code(Box::new(code));
+
+    let addr = app
+        .instantiate_contract(
+            code_id,
+            Addr::unchecked("owner"),
+            &InstantiateMsg { orders: vec![] },
+            &[],
+            "Contract",
+            None,
+        )
+        .unwrap();
+
+    let resp = app
+        .execute_contract(
+            Addr::unchecked("user"),
+            addr.clone(),
+            &ExecuteMsg::CreateOrder {
+                order_type: OrderType::TakeProfit,
+                stop_price: 255,
+                selling_denom: "btc".to_owned(),
+            },
+            &coins(45, "eth"),
+        )
+        .unwrap();
+
+    assert_eq!(
+        app.wrap()
+            .query_balance("user", "eth")
+            .unwrap()
+            .amount
+            .u128(),
+        105
+    );
+
+    assert_eq!(
+        app.wrap()
+            .query_balance(&addr, "eth")
+            .unwrap()
+            .amount
+            .u128(),
+        45
+    );
+
+    let expected_event = Event::new("wasm").add_attribute("action", "create an order");
+    assert_eq!(resp.has_event(&expected_event), true);
+
 }
