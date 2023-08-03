fa879e6fb7b74147d0a3bf590cd95ead20da240a
politeWall <138504353+politeWall@users.noreply.github.com>
2023-08-03 15:28:45 +0200

    test: test: Token transfer success & order ID returned after processing

---
+++ src/tests/process_order/successful_process_take_profit_order.rs
@@ -0,0 +1,84 @@
+use super::*;
+
+// in this exemple we assume :
+//  Bitcoin have a value of 15
+//  Ethereum have a value of 5
+
+#[test]
+fn successful_process_stop_loss_order() {
+    let mut app = App::new(|router, _, storage| {
+        router
+            .bank
+            .init_balance(storage, &Addr::unchecked("user"), coins(150, "eth"))
+            .unwrap();
+
+        router
+            .bank
+            .init_balance(storage, &Addr::unchecked("owner"), coins(1200, "btc"))
+            .unwrap();
+    });
+
+    let dummy_order = Order::new(
+        OrderType::TakeProfit,
+        coin(4, "eth"),
+        coin(600, "btc"),
+        Addr::unchecked("user"),
+        &vec![],
+    );
+
+    let instantiate_msg = InstantiateMsg {
+        orders: vec![dummy_order.clone()],
+    };
+
+    let code = ContractWrapper::new(execute, instantiate, query);
+    let code_id = app.store_code(Box::new(code));
+
+    let addr = app
+        .instantiate_contract(
+            code_id,
+            Addr::unchecked("owner"),
+            &instantiate_msg,
+            &coins(1200, "btc"),
+            "Contract",
+            None,
+        )
+        .unwrap();
+
+    let resp = app
+        .execute_contract(
+            addr.clone(),
+            addr.clone(),
+            &ExecuteMsg::ProcessOrder {},
+            &[],
+        )
+        .unwrap();
+
+    assert_eq!(
+        app.wrap()
+            .query_balance(&addr, "btc")
+            .unwrap()
+            .amount
+            .u128(),
+        600
+    );
+
+    assert_eq!(
+        app.wrap()
+            .query_balance("user", "eth")
+            .unwrap()
+            .amount
+            .u128(),
+        350
+    );
+
+    let refunded_id: Option<u128> = resp.events.iter().find_map(|e| {
+        e.attributes
+            .iter()
+            .find(|attr| {
+                attr.key == "refunded_order_id" && attr.value == dummy_order.order_id.to_string()
+            })
+            .and_then(|attr| attr.value.parse::<u128>().ok())
+    });
+
+    assert!(refunded_id.is_some());
+}
