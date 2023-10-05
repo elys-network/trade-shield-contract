ab93f6fb5ea719c5ded59022d80a2e7665f11c0a
politeWall <138504353+politeWall@users.noreply.github.com>
2023-10-05 11:57:40 +0200

    Front-end Js script to integrate the Cw endpoint to the App (#15)
    * feat: front end js scipt to comunicat with the endpoint of the smart contract
    
    #427
    
    * feat: add variable to the function create order update .gitignore
    
    * docs: add a readme.md to the front_end_script

---
+++ front_end_script/upload.js
@@ -0,0 +1,106 @@
+const {
+  calculateFee,
+  GasPrice,
+  isMsgSubmitProposalEncodeObject,
+} = require("@cosmjs/stargate");
+const { DirectSecp256k1HdWallet, coins } = require("@cosmjs/proto-signing");
+const {
+  SigningCosmWasmClient,
+  CosmWasmClient,
+} = require("@cosmjs/cosmwasm-stargate");
+const _ = require("fs");
+
+// const rpcEndpoint = "https://osmosis-testnet-rpc.polkachu.com:443";
+const rpcEndpoint = "http://localhost:26657";
+
+const sender = {
+  mnemonic:
+    "olympic slide park figure frost benefit deer reform fly pull price airport submit monitor silk insect uphold convince pupil project ignore roof warfare slight",
+  address: "elys12tzylat4udvjj56uuhu3vj2n4vgp7cf9fwna9w",
+};
+
+const GASPRICE = "0.05uelys";
+
+const trade_shield_contract_addr =
+  "elys14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s3fsthx";
+
+async function getOrder(order_id) {
+  const sender_wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+    sender.mnemonic,
+    { prefix: "elys" }
+  );
+  const sender_client = await SigningCosmWasmClient.connectWithSigner(
+    rpcEndpoint,
+    sender_wallet
+  );
+  const result = await sender_client.queryContractSmart(
+    trade_shield_contract_addr,
+    {
+      get_order: { order_id: order_id },
+    }
+  );
+  console.log(`Result: `, result);
+}
+
+async function createOrder(
+  order_amm_routes,
+  order_price,
+  order_type,
+  amount_send,
+  denom_send
+) {
+  const gasPrice = GasPrice.fromString(GASPRICE);
+  const sender_wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+    sender.mnemonic,
+    { prefix: "elys" }
+  );
+  const sender_client = await SigningCosmWasmClient.connectWithSigner(
+    rpcEndpoint,
+    sender_wallet
+  );
+  const executeFee = calculateFee(300_000, gasPrice);
+  const msg = {
+    create_order: {
+      order_amm_routes: order_amm_routes,
+      order_price: order_price,
+      order_type: order_type,
+    },
+  };
+
+  const create_order_res = await sender_client.execute(
+    sender.address,
+    trade_shield_contract_addr,
+    msg,
+    executeFee,
+    "",
+    coins(amount_send, denom_send)
+  );
+  console.log("create_order_res:", create_order_res);
+}
+
+async function cancelOrder(order_id) {
+  const gasPrice = GasPrice.fromString(GASPRICE);
+  const sender_wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+    sender.mnemonic,
+    { prefix: "elys" }
+  );
+  const sender_client = await SigningCosmWasmClient.connectWithSigner(
+    rpcEndpoint,
+    sender_wallet
+  );
+  const executeFee = calculateFee(300_000, gasPrice);
+  const msg = {
+    cancel_order: {
+      order_id: order_id,
+    },
+  };
+
+  const create_order_res = await sender_client.execute(
+    sender.address,
+    trade_shield_contract_addr,
+    msg,
+    executeFee,
+    ""
+  );
+  console.log("create_order_res:", create_order_res);
+}
