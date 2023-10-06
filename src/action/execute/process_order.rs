dbe198d236da5f5a009c6d6cfc32beefd2bdf9cc
politeWall <138504353+politeWall@users.noreply.github.com>
2023-10-06 09:59:16 +0200

    feat: amm swap integration

--- src/action/execute/process_order.rs
+++ src/action/execute/process_order.rs
@@ -5,20 +5,9 @@ use crate::{bindings::query::ElysQuery, msg::ExecuteMsg};
 use super::*;
 
 pub fn process_order(
-    deps: DepsMut<ElysQuery>,
+    _deps: DepsMut<ElysQuery>,
     _info: MessageInfo,
-    env: Env,
+    _env: Env,
 ) -> Result<Response<ElysMsg>, ContractError> {
-    let orders = ORDER.load(deps.storage)?;
-    let order = orders[0].clone();
-
-    let swap_msg = ElysMsg::swap_exact_amount_in(
-        env.contract.address.as_str(),
-        order.order_amount,
-        order.order_amm_routes,
-    );
-
-    let sub_msg = SubMsg::reply_always(swap_msg, 1);
-
-    Ok(Response::new().add_submessage(sub_msg))
+    Ok(Response::new())
 }
