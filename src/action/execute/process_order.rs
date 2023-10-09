70074387503168ed272794d3d97f09cf1b243fb7
politeWall <138504353+politeWall@users.noreply.github.com>
2023-10-09 16:56:04 +0200

    feat: remove the debug code from the process endpoint

--- src/action/execute/process_order.rs
+++ src/action/execute/process_order.rs
@@ -1,24 +1,11 @@
-use cosmwasm_std::SubMsg;
-
 use crate::bindings::query::ElysQuery;
 
 use super::*;
 
 pub fn process_order(
-    deps: DepsMut<ElysQuery>,
+    _deps: DepsMut<ElysQuery>,
     _info: MessageInfo,
-    env: Env,
+    _env: Env,
 ) -> Result<Response<ElysMsg>, ContractError> {
-    let orders = ORDER.load(deps.storage)?;
-    let order = orders[0].clone();
-
-    let swap_msg = ElysMsg::swap_exact_amount_in(
-        env.contract.address.as_str(),
-        order.order_amount,
-        order.order_amm_routes,
-    );
-
-    let sub_msg = SubMsg::new(CosmosMsg::Custom(swap_msg));
-
-    Ok(Response::new().add_submessage(sub_msg))
+    Ok(Response::new())
 }
