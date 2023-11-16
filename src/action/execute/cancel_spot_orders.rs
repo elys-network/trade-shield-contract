12bc1d6ad7d5fe1ed71524865b829f08105f23b5
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-11-16 05:11:34 +0100

    Update cancel_spot_orders.rs

--- src/action/execute/cancel_spot_orders.rs
+++ src/action/execute/cancel_spot_orders.rs
@@ -85,7 +85,7 @@ fn filter_order_by_id(
             .cloned()
             .collect();
 
-        return Err(ContractError::SpotOrdersNotFound {
+        return Err(ContractError::OrderNotFound {
             order_ids: missing_order_ids,
         });
     }
