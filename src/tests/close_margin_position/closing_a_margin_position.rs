cffa799109c5c4f4850067cdcbfe3be758daedd0
politeWall <138504353+politeWall@users.noreply.github.com>
2023-11-24 17:57:14 +0100

    merge fix

--- src/tests/close_margin_position/closing_a_margin_position.rs
+++ src/tests/close_margin_position/closing_a_margin_position.rs
@@ -7,7 +7,6 @@ fn closing_marging_position() {
 
     // Create a mock message to instantiate the contract with no initial orders.
     let instantiate_msg = InstantiateMockMsg {
-        process_order_executor: "owner".to_string(),
         spot_orders: vec![],
         margin_orders: vec![],
     };
