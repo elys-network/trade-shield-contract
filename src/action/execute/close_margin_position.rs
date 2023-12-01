20601e73148662a9db2101e95717a9f6b6e99591
kenta-elys <kenta@elys.network>
2023-12-01 14:49:02 +0000

    fix: address in close margin position

--- src/action/execute/close_margin_position.rs
+++ src/action/execute/close_margin_position.rs
@@ -5,7 +5,7 @@ pub fn close_margin_position(
     env: Env,
     id: u64,
 ) -> Result<Response<ElysMsg>, ContractError> {
-    let msg = ElysMsg::margin_broker_close_position(info.sender, id, env.contract.address);
+    let msg = ElysMsg::margin_broker_close_position(env.contract.address, id, info.sender);
 
     let resp = Response::new().add_message(CosmosMsg::Custom(msg));
 
