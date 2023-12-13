9eebd0b5ebf6d3f749b1295c196a74d438f3fdca
politeWall <138504353+politeWall@users.noreply.github.com>
2023-12-13 15:15:57 +0100

    fix: migration compilation fix (#63)

--- src/entry_point/migrate.rs
+++ src/entry_point/migrate.rs
@@ -3,11 +3,6 @@ use cosmwasm_std::Empty;
 use super::*;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn migrate(
-    _deps: DepsMut<ElysQuery>,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
-) -> StdResult<Response<ElysMsg>> {
+pub fn migrate(_deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
     Ok(Response::new())
 }
